6774b40f482fed39c5affdaa9219634f90a3fb97
Kamila Turek <kamila.turek@gmail.com>
2010-04-28 14:52:11 +0000

    Added msg interface.

---
+++ src/client/MsgClientInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client;
+
+import clientserver.Message;
+
+/**
+ *
+ * @author alimka
+ */
+public interface MsgClientInterface {
+
+    public void showMessage(Message msg);
+
+}
